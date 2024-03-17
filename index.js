feb2d80c268f79a05c1661962dd7e56c6bd96923
AdeyeyeDarasimi <adeyeyedarasimi9@gmail.com>
2024-03-17 14:48:59 -0700

    first commit

---
+++ index.js
@@ -0,0 +1,89 @@
+//console.log("HELLO");
+//console.log("i like pizza ");
+
+//window.alert(`this is an alert`); 
+//window.alert(`i like pizza`); \
+
+
+//document.getElementById("my main").textContent = "hello";
+//document.getElementById("mypara").textContent = "i like pizza";
+
+// this is a comment
+/*
+ this a comment */
+
+ //let age = 25;
+ //let price = 10.99;
+ //let gpa = 2.1;
+
+
+//console.log(typeof price);
+ //console.log(`your are ${age} years old`);
+ //console.log(`the price ${price}`);
+ //log(`Your gpa is ${gpa}`);
+
+ //let FirstName = "Dara";
+ //let favouriteFood = "pizza";
+ //let email = "adeeyeyedarasimi9@gmail.com";
+
+ //console.log(typeof FirstName);
+
+ //console.log(`your name is ${FirstName}`);
+
+ //console.log(`Your email is ${email}`);
+ 
+
+//let fullName = "Adeyeye Darasimi";
+//let age = "21";
+//let student = true;
+
+//document.getElementById("p1").textContent = `Your name is ${fullName}`;
+//document.getElementById("p2").textContent =`you are ${age} years old`;
+//document.getElementById("p3").textContent =`entrolled: ${student}`;
+
+//let student = 40;
+
+//student = student + 1;
+//student = student - 1;
+//student = student * 3
+//student = student % 2;
+//student += 2;
+//student -= 2;
+
+
+//console.log(student);
+
+/* let username;
+
+document.getElementById("submit").onclick = function(){
+     
+    username = document.getElementById("mytext").value;
+    document.getElementById("myh1").textContent =`hello ${username}`;
+    console.log(username);
+}
+*/
+
+/* let age = window.prompt("how old are you?");
+age = Number(age);
+age+=1;
+
+console.log(age);
+*/
+
+
+/* const PI = 3.14159;
+
+let radius;
+let circumference;
+
+
+
+
+
+document.getElementById("submit").onclick = function(){
+    radius =  document.getElementById("mytext").value;
+    radius = Number(radius);
+    circumference = 2 * PI * radius;
+    document.getElementById("myh3").textContent = circumference + "cm";
+}
+*/
